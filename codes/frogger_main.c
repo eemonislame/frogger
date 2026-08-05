ae9f8382ea2310812d4a70274ae0bba33fb3f522
eemonislame <129908853+eemonislame@users.noreply.github.com>
2026-08-06 01:38:46 +0600

    Create frogger_main.c with initial comments
    Add initial structure and guidelines for frogger_main.c

---
+++ codes/frogger_main.c
@@ -0,0 +1,13 @@
+/*
+  this file only gets updated after we both agrees that the chage shoulb be made, other wise not.
+
+  you can update the code in your own file frogger_yourname.c , other user will review it and decide whether it's perfect or not.
+
+*/
+
+
+#inlcude"raylib.h"
+int main()
+{
+
+}
